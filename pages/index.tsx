e44cdcf4b1aa562a5c0d299c218a77dbe378c88e
Lane Garner <b.lane.garner@gmail.com>
2023-03-01 23:03:01 -0600

    remove font import

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import hero from '../public/izzy.jpg'
 import { FaInstagram, FaTiktok } from 'react-icons/fa';
-import { Button } from '@/components/button'
+// import { Button } from '@/components/button'
 
 // const inter = Inter({ subsets: ['latin'] })
 
