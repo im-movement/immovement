787b74c5b3f668c644cce6c9292553d86fa0078f
Lane Garner <b.lane.garner@gmail.com>
2023-03-01 23:27:23 -0600

    install sass and adjust hero image max width

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from '@/styles/Button.module.css'
+import styles from '@/styles/Button.module.scss'
 
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import styles from '@/styles/Home.module.scss'
 import hero from '../public/izzy.jpg'
 import { FaInstagram, FaTiktok } from 'react-icons/fa';
 // import { Button } from '@/components/button'
