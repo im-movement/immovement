e0af9dd3a8582cfb1101a6b50ebfc812f1731d43
Lane Garner <b.lane.garner@gmail.com>
2023-08-29 23:37:25 -0500

    layout wip

---
+++ app/components/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from '@/styles/Header.module.scss';
+import Logo from './Logo';
+import { FaInstagram } from 'react-icons/fa';
+import Link from 'next/link';
+
+const LINKS = [
+  { title: 'Events', href: '/events' },
+  { title: 'Blog', href: '/blog' },
+  { title: 'About', href: '#about' },
+  { title: 'Work with me', href: '#work-with-me' },
+];
+
+export const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <Logo />
+        <h1>IM Movement</h1>
+      </div>
+      <nav>
+        {LINKS.map(l => (
+          <Link href={l.href}>{l.title}</Link>
+        ))}
+      </nav>
+      {/* <FaInstagram className={styles.social} size="2rem" color="white" /> */}
+    </header>
+  );
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '@/styles/Home.module.scss';
-import { FaInstagram, FaTiktok } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { Header } from './components/Header';
 // import { Button } from '@/components/button'
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -15,32 +16,46 @@ const Home = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <section className={styles.mainContainer}>
-          <Image
-            className={styles.hero}
-            src="/izzy.jpg"
-            alt="Isabelle wearing all white standing under two chevron neon lights; one light is red and one is blue"
-            width={1290}
-            height={1609}
-            layout="responsive"
-            // height={37}
-            // priority
-          />
-          <h1 className={styles.heading}>im movement</h1>
-          <h2 className={styles.subHeading}>coming soon...</h2>
-          {/* <Button title="sign up" onClick={()=>alert('sign up')} /> */}
-        </section>
-        <section className={styles.social}>
-          <a
-            href="https://instagram.com/isabellecmuller"
-            rel="noreferer"
-            target="_blank">
-            <FaInstagram size="2rem" color="white" />
-          </a>
-          {/* <FaTiktok color="white" /> */}
-        </section>
-      </main>
+      <div className={styles.backgroundContainer}>
+        <Header />
+        <main className={styles.main}>
+          <section className={styles.screen}>
+            <Image
+              className={styles.hero}
+              src="/izzy.jpg"
+              alt="Isabelle wearing all white standing under two chevron neon lights; one light is red and one is blue"
+              width={1290}
+              height={1609}
+              placeholder="blur"
+              blurDataURL="/izzy.jpg"
+              layout="responsive"
+              // height={37}
+              // priority
+            />
+            <h1 className={styles.heading}>IM Movement</h1>
+            <h2 className={styles.subHeading}>coming soon...</h2>
+            {/* <Button title="sign up" onClick={()=>alert('sign up')} /> */}
+          </section>
+          <section className={styles.screen}>
+            <h2>Upcoming events</h2>
+          </section>
+          <section className={styles.screen}>
+            <h2>Blog</h2>
+          </section>
+          <section className={styles.screen} id="work-with-me">
+            <h2>Work with me</h2>
+          </section>
+          <section className={styles.screen}>
+            <h2>Follow me on Instagram</h2>
+            <a
+              href="https://instagram.com/isabellecmuller"
+              rel="noreferer"
+              target="_blank">
+              <FaInstagram size="2rem" color="white" />
+            </a>
+          </section>
+        </main>
+      </div>
     </>
   );
 };
