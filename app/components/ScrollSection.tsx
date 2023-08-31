be851dfd366d669113c65533bb55dcc24845dec6
Lane Garner <b.lane.garner@gmail.com>
2023-08-30 19:43:29 -0500

    try adding use client to containing component

---
+++ app/components/ScrollSection.tsx
@@ -0,0 +1,29 @@
+'use client';
+import React, { useEffect } from 'react';
+
+export const ScrollSection: React.FC = () => {
+  useEffect(() => {
+    let disableScroll = false;
+    const scrollContainer = document.querySelector('main');
+    const grabScroll = (e: Event) => {
+      e.preventDefault();
+      if (!disableScroll) {
+        if (scrollContainer) {
+          disableScroll = true;
+          if (disableScroll) {
+            scrollContainer.scrollLeft += scrollContainer.clientWidth - 40;
+            setTimeout(() => {
+              disableScroll = false;
+            }, 300);
+          }
+        }
+      }
+    };
+    scrollContainer?.addEventListener('wheel', grabScroll, { passive: false });
+    return () => {
+      scrollContainer?.removeEventListener('wheel', grabScroll);
+    };
+  }, []);
+
+  return <></>;
+};
