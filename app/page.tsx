5baf85062ca392c4e84ad4a7a0100ab9711a9f7b
Lane Garner <b.lane.garner@gmail.com>
2023-09-06 22:57:11 -0500

    image placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,20 @@ import styles from '@/styles/Home.module.scss';
 import { FaInstagram } from 'react-icons/fa';
 import { MainLayout } from './components/MainLayout';
 
+const keyStr =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+const triplet = (e1: number, e2: number, e3: number) =>
+  keyStr.charAt(e1 >> 2) +
+  keyStr.charAt(((e1 & 3) << 4) | (e2 >> 4)) +
+  keyStr.charAt(((e2 & 15) << 2) | (e3 >> 6)) +
+  keyStr.charAt(e3 & 63);
+
+const rgbDataURL = (r: number, g: number, b: number) =>
+  `data:image/gif;base64,R0lGODlhAQABAPAA${
+    triplet(0, r, g) + triplet(b, 255, 255)
+  }/yH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==`;
+
 const Home = () => {
   return (
     <MainLayout horizontal>
@@ -18,10 +32,9 @@ const Home = () => {
           width={1290}
           height={1609}
           placeholder="empty"
-          blurDataURL="/izzy.jpg"
+          blurDataURL={rgbDataURL(79, 78, 74)}
           priority
           layout="responsive"
-          // height={37}
         />
         <h2 className={styles.subHeading}>coming soon...</h2>
       </section>
