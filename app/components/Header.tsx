b2a95f8fdb5a7c7922b0a8bac3f5ca3cf70933f2
Lane Garner <b.lane.garner@gmail.com>
2023-09-06 21:33:04 -0500

    add mobile menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import { FaInstagram } from 'react-icons/fa';
 import Link from 'next/link';
 
 import Logo from './Logo';
-import styles from '@/styles/Header.module.scss';
-import { oswald } from '@/styles/fonts';
+import { Nav } from './Nav';
 
-const LINKS = [
-  { title: 'Events', href: '/events' },
-  { title: 'Blog', href: '/blog' },
-  { title: 'About', href: '#about' },
-  { title: 'Work with me', href: '#work-with-me' },
-];
+import styles from '@/styles/Header.module.scss';
 
 export const Header: React.FC = () => {
   return (
@@ -19,16 +12,7 @@ export const Header: React.FC = () => {
       <Link href={'/'}>
         <Logo />
       </Link>
-      <div className={styles.navContainer}>
-        <nav className={oswald.className}>
-          {LINKS.map(l => (
-            <Link href={l.href}>{l.title}</Link>
-          ))}
-        </nav>
-        <a href="https://www.instagram.com/isabellecmuller/" target="_blank">
-          <FaInstagram size="2rem" color="white" />
-        </a>
-      </div>
+      <Nav />
     </header>
   );
 };
---
+++ app/components/Nav.tsx
@@ -0,0 +1,60 @@
+'use client';
+import React from 'react';
+import Link from 'next/link';
+
+import styles from '@/styles/Header.module.scss';
+
+import Hamburger from 'hamburger-react';
+import { oswald } from '@/styles/fonts';
+import { FaInstagram } from 'react-icons/fa';
+
+const LINKS = [
+  { title: 'Events', href: '/events' },
+  { title: 'Blog', href: '/blog' },
+  { title: 'About', href: '#about' },
+  { title: 'Work with me', href: '#work-with-me' },
+];
+
+const MobileNav: React.FC = () => {
+  const setMenuPosition = (e: boolean) => {
+    const position = e ? '0' : '-100vw';
+    document.documentElement.style.setProperty(
+      '--menu-position',
+      `${position}`,
+    );
+  };
+
+  return (
+    <div className={styles.mobileNavContainer}>
+      <Hamburger
+        rounded
+        label="Show menu"
+        color="white"
+        onToggle={setMenuPosition}
+      />
+      <div className={styles.mobileNav}>
+        {LINKS.map(l => (
+          <Link href={l.href}>{l.title}</Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export const Nav: React.FC = () => {
+  return (
+    <>
+      <div className={styles.navContainer}>
+        <nav className={oswald.className}>
+          {LINKS.map(l => (
+            <Link href={l.href}>{l.title}</Link>
+          ))}
+        </nav>
+        <a href="https://www.instagram.com/isabellecmuller/" target="_blank">
+          <FaInstagram size="2rem" color="white" />
+        </a>
+      </div>
+      <MobileNav />
+    </>
+  );
+};
