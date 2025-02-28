673f6da782c259484cedc628f169ea4b01f3b1d1
Lane Garner <b.lane.garner@gmail.com>
2025-02-28 01:29:58 -0600

    update events card image

--- app/components/Images/EventsImage.tsx
+++ app/components/Images/EventsImage.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import styles from '@/styles/Home.module.scss';
-import img from '../../../public/events.png';
+import img from '../../../public/red-background.png';
 
 export const EventsImage = () => {
   return (
